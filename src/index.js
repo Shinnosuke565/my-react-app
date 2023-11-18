0903f24a368fb98bf3740f457568e1030e43fa93
shinnosuke565 <damking420@gmail.com>
2023-11-18 12:30:36 +0900

    Your commit message here

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import NewTodoApp from './NewTodoApp';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <NewTodoApp /> {}
-  </React.StrictMode>,
-  document.getElementById('root')
-);
